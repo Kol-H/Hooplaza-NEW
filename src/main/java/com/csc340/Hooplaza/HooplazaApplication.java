f5c1b4bdcf719189ac6c9294e372a9692cf093f8
Kol-H <arherget@uncg.edu>
2024-02-28 12:46:23 -0500

    add all files from archived proj (original owner: lcroberts)

---
+++ src/main/java/com/csc340/Hooplaza/HooplazaApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.Hooplaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooplazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooplazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340/Hooplaza/HooplazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.Hooplaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooplazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
